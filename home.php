3a0af0d8fa593fd2d12d3f46b8e4e38fa8604309
Techin-arch <puiuria64@gmail.com>
2025-12-19 09:57:00 +0700

    Edit file#1

--- home.php
+++ home.php
@@ -22,7 +22,7 @@
 			</td>
 			<td width="190" bgcolor="#FFFF66"><ul>
 				<li><a href="home.php">หน้าแรก</li>
-				<li><a href="room.php">ห้องพัก&ราคาย่อมเยา</li>
+				<li><a href="room.php">ห้องพัก&ราคาย่อมเยาวราช</li>
 				<li><a herf="guest.php">สมุดเยี่ยม</li>
 				<li><a href="contact.php">ติดต่อยยยยย</li>
 				</ul><p>
